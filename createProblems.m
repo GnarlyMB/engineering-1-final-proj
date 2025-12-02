3f32e3fd5ce00ff63d6857a274915064a27fe4d9
b0shi <b0shi@b0shi-ThinkPad-P16s-Gen-4-AMD>
2025-12-02 09:41:22 -0500

    test

--- createProblems.m
+++ createProblems.m
@@ -7,13 +7,31 @@ classdef createProblems
     end
 
     methods
-        function [num1, num2] = createNumbers()
-            num1 = randi(10)
-            num2 = randi(10)
+        function [num1] = createNumbers()
+            num1 = randi(10);
         end
 
-        function output = buildProblem(num1, num2)
+        function [correct] = buildCorrect(num1, num2)
             %build the numbers + answer
+            correct = num1 * num2
+        end
+
+        function [wrong] = buildWrong(correct)
+            possible = randi(100)
+            if possible == correct
+                buildWrong(correct)
+            else
+                wrong = possible
+            end
+        end
+
+        function output = buildOutput()
+            number1 = createNumbers();
+            number2 = createNumbers();
+            correct = buildCorrect(number1, number2)
+            wrong1 = buildWrong(correct)
+            wrong2 = buildWrong(correct)
+            output = [correct, wrong1, wrong2]
         end
     end
 end
--- main.m
+++ main.m
@@ -4,4 +4,4 @@ clear all
 
 sprite = simpleGameEngine('retro_pack.png', 16, 16, 3, [0, 0, 0]);
 
-createProblem()
+buildOutput()
