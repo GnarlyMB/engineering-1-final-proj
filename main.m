e17d3775fb567243a67519134064940f1523e5cb
oscar <oscar@OCPXPS>
2025-12-04 15:32:37 -0500

    committing

--- main.m
+++ main.m
@@ -3,6 +3,10 @@ close
 clear all
 
 startingScreen = simpleGameEngine('retro_pack.png', 16, 16, 3, [0, 0, 0]);
+input = [1000, 1000];
+while input(x) == 1000 & input(y) == 1000
+    input = getMouseInput(startingScreen);
+end
 
 b = createProblems;
 
