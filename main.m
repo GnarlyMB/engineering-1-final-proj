389585b9a41512dd710413dd225b47c0b9f590c5
oscar <oscar@OCPXPS>
2025-12-04 15:28:06 -0500

    commit

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clc
 close 
 clear all
 
-sprite = simpleGameEngine('retro_pack.png', 16, 16, 3, [0, 0, 0]);
+startingScreen = simpleGameEngine('retro_pack.png', 16, 16, 3, [0, 0, 0]);
 
 b = createProblems;
 
--- questionActivateFunction.m
+++ questionActivateFunction.m
@@ -1,6 +1,6 @@
 function [i, j] = questionActivateFunction(i, j, qPosI, qPosJ)
-%questionActivateFunction - checks whether the 
-%   Detailed explanation goes here
+%questionActivateFunction - checks whether the player's position matches
+%any question position
     if ismember(i, qPosI)
         index = find(qPosI == i);
         if ismember(j, qPosJ)
